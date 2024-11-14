29291d5cad34210b42f9f4254a67af4bbe97f6a7
varun646 <varun646@gmail.com>
2024-11-14 11:00:47 -0500

    python notebook, script for LDA
    - ipynb for LDA
    - python script for LDA

---
+++ existing_code/octis_lda_topic_modeling.py
@@ -0,0 +1,74 @@
+from octis.models.LDA import LDA
+import pandas as pd
+import string
+from octis.preprocessing.preprocessing import Preprocessing
+
+print("reading data...")
+
+# read data
+data_say_hello = pd.read_csv('/Users/vnarayan35/Documents/GitHub/PatientX.AI/existing_code/forum-crawler data/Say hello and introduce yourself.csv')
+data_recently_diagnosed = pd.read_csv('/Users/vnarayan35/Documents/GitHub/PatientX.AI/existing_code/forum-crawler data/Recently diagnosed and early stages of dementia.csv')
+data_memory_concerns = pd.read_csv("/Users/vnarayan35/Documents/GitHub/PatientX.AI/existing_code/forum-crawler data/Memory concerns and seeking a diagnosis.csv")
+data_i_have_dementia = pd.read_csv("/Users/vnarayan35/Documents/GitHub/PatientX.AI/existing_code/forum-crawler data/I have dementia.csv")
+data_i_have_partner = pd.read_csv("/Users/vnarayan35/Documents/GitHub/PatientX.AI/existing_code/forum-crawler data/I have a partner with dementia.csv")
+data_i_care = pd.read_csv("/Users/vnarayan35/Documents/GitHub/PatientX.AI/existing_code/forum-crawler data/I care for a person with dementia.csv")
+
+
+print("read data")
+
+
+# combine data into single dataframe
+dfs = [data_say_hello, data_recently_diagnosed, data_memory_concerns, data_i_have_dementia, data_i_have_partner, data_i_care]
+forum_data_union = pd.concat(dfs, ignore_index=True)
+
+
+SAMPLE_SIZE = 50
+
+sample_data = forum_data_union.sample(SAMPLE_SIZE)
+
+print("sampled")
+
+# Save to CSV file
+
+# save to TSV file, train, test, val splits
+sample_data.to_csv(path_or_buf='/Users/vnarayan35/Documents/GitHub/PatientX.AI/existing_code/dataset/sample_data_final_fixed.tsv', index=False, sep='\t')
+
+# data that we will use for LDA
+data_review = forum_data_union['post_message'] # hold only text from posts
+data_review.to_csv(r'./corpus.txt', header=None, index=None, sep='\n', mode='a')
+
+print("preprocessing...")
+
+# preprocessing - remove whitespace, remove punctuation, convert to lowercase
+preprocessor = Preprocessing(vocabulary=None, max_features=None,
+                             remove_punctuation=True, punctuation=string.punctuation,
+                             lemmatize=True, stopword_list='english',
+                             min_chars=1, min_words_docs=0)
+
+dataset = preprocessor.preprocess_dataset(documents_path=r'./corpus.txt')
+print("done preprocessing")
+
+print("saving...")
+dataset.save(path='./processed_dataset/')
+print("done saving")
+
+
+
+# Topic modeling
+
+model = LDA(num_topics=2, alpha=0.1)
+
+
+print("training lda...")
+
+# Train the model using default partitioning choice
+output = model.train_model(dataset)
+
+print("done training")
+
+print(*list(output.keys()), sep="\n") # Print the output identifiers
+
+for t in output['topics'][:5]:
+  print(" ".join(t))
+
+
