eebee5798307f63cf35b6a298cd462de8246eb79
varun646 <varun646@gmail.com>
2024-11-21 13:59:26 -0500

    add return types to interface

--- src/ClusteringModelInterface.py
+++ src/ClusteringModelInterface.py
@@ -1,19 +1,22 @@
 from abc import ABC, abstractmethod
 from os import PathLike
+from typing import Union
+
+import pandas as pd
 
 
 class ClusteringModelInterface(ABC):
     @abstractmethod
-    def getClusters(self, datapath: PathLike):
+    def getClusters(self, datapath: PathLike) -> Union[dict, pd.DataFrame]:
         '''
 
         :param datapath: path to txt file holding documents separated by '\n'
-        :return: topic modeling results
+        :return: Union[dict, pd.DataFrame]: return results of topic modeling
         '''
         pass
 
     @abstractmethod
-    def getModelType(self):
+    def getModelType(self) -> str:
         '''
         :return: string describing model type
         '''
